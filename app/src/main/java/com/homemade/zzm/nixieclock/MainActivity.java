ca8ec1ae3b2eac72173ec4dfb656eb41255cbe96
邹哲明 <zzmhacker@gmail.com>
2017-06-08 10:47:06 +0800

    增加了老虎机动态的效果

--- app/src/main/java/com/homemade/zzm/nixieclock/MainActivity.java
+++ app/src/main/java/com/homemade/zzm/nixieclock/MainActivity.java
@@ -25,6 +25,26 @@ public class MainActivity extends AppCompatActivity {
     private Button luckybtn;
     private boolean result_out1 = false, result_out2 = false, result_out3 = false;
     private int num1, num2, num3;
+    private boolean blurflag1 = false, blurflag2 = false, blurflag3 = false;
+    private Handler blurHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    onearmbandit1.setImageResource(CommonUtils.getPictureResource((char) (48 + getARandomNum0_9())));
+                    break;
+                case 2:
+                    onearmbandit2.setImageResource(CommonUtils.getPictureResource((char) (48 + getARandomNum0_9())));
+                    break;
+                case 3:
+                    onearmbandit3.setImageResource(CommonUtils.getPictureResource((char) (48 + getARandomNum0_9())));
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
     private Handler gameHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -35,21 +55,27 @@ public class MainActivity extends AppCompatActivity {
                     result_out1 = false;
                     result_out2 = false;
                     result_out3 = false;
-                    gameHandler.sendEmptyMessageDelayed(STEP_2_PIC3OK, (int) (Math.random() * 2000));
-                    gameHandler.sendEmptyMessageDelayed(STEP_3_PIC2OK, (int) (Math.random() * 2000));
-                    gameHandler.sendEmptyMessageDelayed(STEP_4_PIC1OK, (int) (Math.random() * 2000));
+                    startBlur(1);
+                    startBlur(2);
+                    startBlur(3);
+                    gameHandler.sendEmptyMessageDelayed(STEP_2_PIC3OK, (int) (Math.random() * 4000));
+                    gameHandler.sendEmptyMessageDelayed(STEP_3_PIC2OK, (int) (Math.random() * 4000));
+                    gameHandler.sendEmptyMessageDelayed(STEP_4_PIC1OK, (int) (Math.random() * 4000));
                     break;
                 case STEP_2_PIC3OK:
+                    stopBlur(3);
                     onearmbandit3.setImageResource(CommonUtils.getPictureResource((char) (48 + num3)));
                     result_out1 = true;
                     check();
                     break;
                 case STEP_3_PIC2OK:
+                    stopBlur(2);
                     onearmbandit2.setImageResource(CommonUtils.getPictureResource((char) (48 + num2)));
                     result_out2 = true;
                     check();
                     break;
                 case STEP_4_PIC1OK:
+                    stopBlur(1);
                     onearmbandit1.setImageResource(CommonUtils.getPictureResource((char) (48 + num1)));
                     result_out3 = true;
                     check();
@@ -69,6 +95,64 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+    private void startBlur(int witch) {
+        switch (witch) {
+            case 1:
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        blurflag1 = true;
+                        while (blurflag1) {
+                            blurHandler.sendEmptyMessage(1);
+                            try {
+                                sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }.start();
+                break;
+            case 2:
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        blurflag2 = true;
+                        while (blurflag2) {
+                            blurHandler.sendEmptyMessage(2);
+                            try {
+                                sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }.start();
+                break;
+            case 3:
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        blurflag3 = true;
+                        while (blurflag3) {
+                            blurHandler.sendEmptyMessage(3);
+                            try {
+                                sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }.start();
+                break;
+            default:
+                break;
+        }
+    }
 
     private void check() {
         if (result_out3 && result_out2 && result_out1) {
@@ -101,11 +185,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateViews() {
-        gameHandler.sendEmptyMessage(STEP_1_START);
-        num1 = (int) (Math.random() * 10);
-        num2 = (int) (Math.random() * 10);
-        num3 = (int) (Math.random() * 10);
+        num1 = getARandomNum0_9();
+        num2 = getARandomNum0_9();
+        num3 = getARandomNum0_9();
         total_time++;
+        gameHandler.sendEmptyMessage(STEP_1_START);
+    }
+
+    private int getARandomNum0_9() {
+        return (int) (Math.random() * 10);
     }
 
     @Override
@@ -115,5 +203,21 @@ public class MainActivity extends AppCompatActivity {
         editor.putInt("success_time", success_time);
         editor.putInt("total_time", total_time);
         editor.apply();
+    }
+
+    private void stopBlur(int witch) {
+        switch (witch) {
+            case 1:
+                blurflag1 = false;
+                break;
+            case 2:
+                blurflag2 = false;
+                break;
+            case 3:
+                blurflag3 = false;
+                break;
+            default:
+                break;
+        }
     }
 }
